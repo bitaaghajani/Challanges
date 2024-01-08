6e5d2ff04e3787e3a6e8e689853b31d530682f15
Bita Aghajani <106587748+bitaaghajani@users.noreply.github.com>
2024-01-08 11:30:06 +0330

    Delete LAWeb.js directory

--- LAWeb.js/app/index.js
+++
@@ -1,9 +0,0 @@
-const slides=document.querySelectorAll('.slider');
-const banner=document.querySelector('.historyBanner')
-
-slides.forEach(item=>{
-    item.addEventListener('click',()=>{
-        console.log('click',item)
-        banner.style.backgroundImage=`url(${item.src})`
-    })
-})
